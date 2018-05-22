6d1e64755ab2c0f931550214503796677d172674
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-05-22 22:30:46 +0200

    Tilføjet glemt kodeord funktion.

---
+++ app/src/main/java/sovsen/plejeren/ForgotpasswordActivity.java
@@ -0,0 +1,61 @@
+package sovsen.plejeren;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotpasswordActivity extends AppCompatActivity {
+
+    // Deklarer en EditText variabel
+    private EditText mEmail_field;
+
+    // Deklarer en Button variabel
+    private Button mReset_button;
+
+    // Deklarer en FirebaseAuth variabel
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgotpassword);
+
+        // Sætter field med id email_field_view til mEmail_field variabel
+        mEmail_field = (EditText) findViewById(R.id.email_field_view);
+
+        // Sætter knappen med id reset_button til mReset_button variabel
+        mReset_button = (Button) findViewById(R.id.reset_button);
+
+        // Henter en instance af Firebase Authentication
+        mAuth = FirebaseAuth.getInstance();
+
+        mReset_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Gemmer email i en String datatype
+                String email = mEmail_field.getText().toString();
+
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(ForgotpasswordActivity.this, "Vi har sendt dig en e-mail med et nulstillings link!", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(ForgotpasswordActivity.this, "Nulstillingen fejlede!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+}
--- app/src/main/java/sovsen/plejeren/MainActivity.java
+++ app/src/main/java/sovsen/plejeren/MainActivity.java
@@ -1,11 +1,13 @@
 package sovsen.plejeren;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     // Deklarer en EditText variabel
     private EditText mEmail_field;
     private EditText mPassword_field;
+
+    // Deklarer en TextView variabel
+    private TextView mForgot_password_view;
 
     // Deklarer en Button variabel
     private Button mLogin_button;
@@ -36,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Sætter field med id password_field_view til mPassword_field variabel
         mPassword_field = (EditText) findViewById(R.id.password_field_view);
+
+        // Sætter view med id forgot_password_view til mForgot_password_view variabel
+        mForgot_password_view = (TextView) findViewById(R.id.forgot_password_view);
 
         // Sætter knappen med id login_button til mLogin_button variabel
         mLogin_button = (Button) findViewById(R.id.login_button);
@@ -56,6 +64,14 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        mForgot_password_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openForgotpassword = new Intent(MainActivity.this, ForgotpasswordActivity.class);
+                startActivity(openForgotpassword);
+            }
+        });
     }
 
     @Override
@@ -71,11 +87,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Login fuldført!", Toast.LENGTH_LONG).show();
                     FirebaseUser user = mAuth.getCurrentUser();
 
                 } else {
-                    Toast.makeText(MainActivity.this, "Login failed!", Toast.LENGTH_LONG).show();
+
+                    Toast.makeText(MainActivity.this, "Login fejlede!", Toast.LENGTH_LONG).show();
                 }
             }
         });
