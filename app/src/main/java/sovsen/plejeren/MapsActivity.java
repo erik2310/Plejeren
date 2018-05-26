f252bb6d47d1a0cd8e39423a67ec634ce407683d
niel905g <niel905g@edu.easj.dk>
2018-05-26 05:40:58 +0200

    Google Maps inkl - Marker in Fåborg!

--- app/src/main/java/sovsen/plejeren/MapsActivity.java
+++ app/src/main/java/sovsen/plejeren/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng fåborg = new LatLng(55.099, 10.233);
+        mMap.addMarker(new MarkerOptions().position(fåborg).title("Marker in Fåborg"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(fåborg));
     }
 }
