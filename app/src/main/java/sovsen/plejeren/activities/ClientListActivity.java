fa55f9ed90f5093f3eca4bfdc22fc92508a10bb8
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-05-29 13:07:12 +0200

    Lavet ClientListActivity som nu bliver åbnet når man klikker på Arbejdsplan.

---
+++ app/src/main/java/sovsen/plejeren/activities/ClientListActivity.java
@@ -0,0 +1,15 @@
+package sovsen.plejeren.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import sovsen.plejeren.R;
+
+public class ClientListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_list);
+    }
+}
