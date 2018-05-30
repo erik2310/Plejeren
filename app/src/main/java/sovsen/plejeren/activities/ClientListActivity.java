9324fb9d20688b094777e60a1a7064083714963a
kasp010k <kasp010k@edu.easj.dk>
2018-05-30 11:23:18 +0200

    Her er MainActivity.class og activity_main.xml
    Har lavet lidt ekstra kode til ClientListActivity og tilføjet Workplan class og xml.

--- app/src/main/java/sovsen/plejeren/activities/ClientListActivity.java
+++ app/src/main/java/sovsen/plejeren/activities/ClientListActivity.java
@@ -1,15 +1,33 @@
 package sovsen.plejeren.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import sovsen.plejeren.R;
 
 public class ClientListActivity extends AppCompatActivity {
 
+    private Button Client_1_Button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_list);
+
+
+        Client_1_Button = (Button) findViewById(R.id.Client_1);
+
+        Client_1_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent openWorkplan = new Intent(ClientListActivity.this, WorkplanActivity.class);
+                startActivity(openWorkplan);
+
+            }
+        });
     }
 }
